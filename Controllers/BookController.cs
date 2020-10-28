5bcf8dc8b007f01165385a6e3e7a87c26f26803d
soloviev-nikolay <soloviev_nikolay@mail.ru>
2020-10-28 23:58:04 +0500

    add

---
+++ Controllers/BookController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp.DataAccess;
+using WebApp.DataAccess.Entities;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class BookController : Controller
+    {
+        public DbContextApp _dbContext;
+        public BookController(DbContextApp dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public Action CreateBook(BookDto book)
+        {
+            var b = new Book()
+            {
+                ID = book.ID,
+                Author = book.Author,
+                ISBN = book.ISBN,
+                Language = book.Language,
+                Pages = book.Pages,
+                Title = book.Title
+            };
+            _dbContext.Books.Add(b);
+            _dbContext.SaveChanges();
+            return;
+        }
+    }
+}
---
+++ Controllers/PublisherController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp.DataAccess;
+using WebApp.DataAccess.Entities;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class PublisherController : Controller
+    {
+        public DbContextApp _dbContext;
+        public PublisherController(DbContextApp dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        
+    }
+}
